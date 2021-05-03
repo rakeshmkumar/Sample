ff29e22adb11d538c81ea580bd9fbec5ada56e2d
rakeshmkumar <67459134+rakeshmkumar@users.noreply.github.com>
2021-05-03 22:45:15 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$con = mysqli_connect('122.164.84.147','root','')
+
+if(!$con){
+	echo 'Not connected to server';
+	}
+
+if(! mysqli_select_db($con,'HAIxhNBolo')){
+	echo 'Database Not Selected';
+	}
+
+$Name = $_POST['name'];
+$address = $_POST['address'];
+$mobile = $_POST['mobile'];
+
+
+$sql="INSERT INTO table (name, addres, mobile) VALUES ('$name', '$address', '$mobile')";
+
+
+if(!mysql_query($con,sql)){
+	echo 'Not inserted';
+	}
+else{
+	echo 'Inserted';
+	}
+
+header("refresh:2; url=index.html");
+
+?>
